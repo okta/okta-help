2dc6db32a4bcc6ce895e1e781cfbb73d47bb6118
juliaruizborys-okta <julia.ruizborys@okta.com>
2017-05-05 11:34:25 -0700

    republished to include updated doc

--- en/prev/Resources/Scripts/require.config.js
+++ en/prev/Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=636294856786708772'
+    urlArgs: 't=636295807208726088'
 });
--- en/prod/Resources/Scripts/require.config.js
+++ en/prod/Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=636294367063083854'
+    urlArgs: 't=636295808426166621'
 });
