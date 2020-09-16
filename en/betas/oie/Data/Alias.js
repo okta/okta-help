8bcc2a1c99ddbd3a3cd4a891cebb7b590be0b530
markcowan <52024619+MarkCowan-okta@users.noreply.github.com>
2020-09-16 14:38:17 -0600

    reset base URL

---
+++ en/betas/oie/Data/Alias.js
@@ -0,0 +1,6 @@
+var xmlAliasData = "";
+xmlAliasData += '<?xml version=\"1.0\" encoding=\"utf-8\"?>';
+xmlAliasData += '<CatapultAliasFile>';
+xmlAliasData += '    <Map Name=\"identity-engine\" Link=\"Topics/identity-engine/oie-index.htm\" ResolvedId=\"1328\" />';
+xmlAliasData += '</CatapultAliasFile>';
+MadCap.Utilities.Xhr._FilePathToXmlStringMap.Add('Alias', xmlAliasData);
---
+++ en/betas/oie/Resources/Scripts/require.config.js
@@ -0,0 +1,3 @@
+require.config({
+    urlArgs: 't=637358638134668294'
+});
