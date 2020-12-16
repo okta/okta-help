4eb75273bf324ff4c79383963d1069a2f5bd0304
damianali-okta <damian.ali@okta.com>
2020-12-16 13:53:57 -0500

    OIE: Release notes for 2020.12.1

--- en/oie/Data/Alias.js
+++ en/oie/Data/Alias.js
@@ -1,7 +1,10 @@
 var xmlAliasData = "";
 xmlAliasData += '<?xml version=\"1.0\" encoding=\"utf-8\"?>';
 xmlAliasData += '<CatapultAliasFile>';
+xmlAliasData += '    <Map Name=\"Security_Network\" Link=\"Topics/Security/Security_Network.htm\" ResolvedId=\"1100\" />';
+xmlAliasData += '    <Map Name=\"ext_Security_Network\" Link=\"Topics/Security/Security_Network.htm\" ResolvedId=\"200172\" />';
 xmlAliasData += '    <Map Name=\"csh_threat_insight\" Link=\"Topics/Security/threat-insight/ti-index.htm\" ResolvedId=\"1116\" />';
+xmlAliasData += '    <Map Name=\"ext_Location_Zones\" Link=\"Topics/Security/Security_Network.htm\" ResolvedId=\"1151\" />';
 xmlAliasData += '    <Map Name=\"csh-threat-insight\" Link=\"Topics/Security/threat-insight/ti-index.htm\" ResolvedId=\"1192\" />';
 xmlAliasData += '    <Map Name=\"ext_threatinsight\" Link=\"Topics/Security/threat-insight/ti-index.htm\" ResolvedId=\"1195\" />';
 xmlAliasData += '    <Map Name=\"csh-ad-import-settings\" Link=\"Topics/Directory/ad-agent-configure-import.htm\" ResolvedId=\"1283\" />';
@@ -15,6 +18,7 @@ xmlAliasData += '    <Map Name=\"csh-oie-upgrade\" Link=\"Topics/identity-engine
 xmlAliasData += '    <Map Name=\"csh-features\" Link=\"Topics/identity-engine/oie-features.htm\" ResolvedId=\"2003\" />';
 xmlAliasData += '    <Map Name=\"csh-device-mgmt-desktop-mdm-setup\" Link=\"Topics/identity-engine/oie-index.htm\" ResolvedId=\"12000\" />';
 xmlAliasData += '    <Map Name=\"csh-about-authenticators\" Link=\"Topics/identity-engine/authenticators/about-authenticators.htm\" ResolvedId=\"2004\" />';
+xmlAliasData += '    <Map Name=\"csh-first-party-apps\" Link=\"Topics/Settings/first-party-apps.htm\" ResolvedId=\"7000\" />';
 xmlAliasData += '    <Map Name=\"csh-device-mgmt-mobile-mdm-setup\" Link=\"Topics/identity-engine/oie-index.htm\" ResolvedId=\"12001\" />';
 xmlAliasData += '</CatapultAliasFile>';
 MadCap.Utilities.Xhr._FilePathToXmlStringMap.Add('Alias', xmlAliasData);
--- en/oie/Resources/Scripts/require.config.js
+++ en/oie/Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=637431360848424148'
+    urlArgs: 't=637437226110971388'
 });
