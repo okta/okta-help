e726ee30d4cb8f4c6efb604e26e01ac823524fc3
Nikhil Venkatraman <nikhil.venkatraman@okta.com>
2021-01-12 13:07:26 -0800

    Convert alias to query param Resolves: OKTA-359902

--- i18n-util/readme.md
+++ i18n-util/readme.md
@@ -1,7 +1,24 @@
 # i18n-util
 
   
-i18n-util package hosts utils to do locale computation and some custom redirection on help.okta.com. An alias url from the monolith in the format `help.okta.com/okta_help.htm#csh-eu-quick-access-apps?locale=ja` will get transformed into `help.okta.com/ja/prod/okta_help.htm#cshid=csh-eu-quick-access-apps`. `locale` is an optional parameter. If locale is not sent, we'll compute it based on a [priority](#locale)
+i18n-util package hosts utils to do locale computation and some custom redirection on help.okta.com. 
+The new help link base url format is now available for use, http://help.okta.com/okta_help.htm?id=${doc_alias}.
+
+Admin links will get redirected to en version of the docs since we don’t have translations yet.
+Example:
+
+http://help.okta.com/okta_help.htm?id=csh-config-asso
+
+End-user links will get redirected to the localized version of the docs since we have translations.
+Example:
+
+https://help.okta.com/okta_help.htm?id=csh_ov_overview&type=end-user
+
+Note:
+ -  `id` (alias id) should be provided as the first query param argument in the url
+ -  Enduser docs will need to include a `type` as end-user in the query param. `type` is currently not required for admin docs and is treated as default.
+ -  `locale` is an optional parameter. For admin docs `locale` will always be computed as en (till we get translations ready).
+ -  For end-user docs `locale` will computed automatically based on a [priority](#locale)
 
 <!-- toc -->
 
@@ -38,7 +55,7 @@ Any time we make changes to src files within `i18n-util`, we need to make sure w
 
   Locale is computed within the LocaleUtil based on a priority list where we look at locale in the following order. A similar priority is used while computing locale for the okta monolith app.
 
- 1. Locale in query param Example help.okta.com/okta_help.htm#csh-eu-quick-access-apps?locale=ja
+ 1. Locale in query param Example help.okta.com/okta_help.htm?id=csh-eu-quick-access-apps&locale=ja
  2. Locale set via cookie
  3. Browser locale
  4. Default to `en`
--- i18n-util/test/spec/UrlUtil_spec.js
+++ i18n-util/test/spec/UrlUtil_spec.js
@@ -1,95 +1,65 @@
-import { getAliasUrl, getRedirectUrl, getUrlParameter } from '../../src/UrlUtil';
-
-describe('UrlUtil.getAliasUrl', () => {
-  test('generates alias url and adds cshid correctly with locale', () => {
-    const result = getAliasUrl({
-      hash: '#csh-eu-quick-access-apps?locale=en',
-      href: 'help.foo.com/okta_help.htm#csh-eu-quick-access-apps?locale=en'
-    });
-    expect(result).toEqual('help.foo.com/okta_help.htm#cshid=csh-eu-quick-access-apps?locale=en');
-  });
-
-  test('generates alias url and adds cshid correctly without locale', () => {
-    const result = getAliasUrl({
-      hash: '#csh-eu-quick-access-apps',
-      href: 'help.foo.com/okta_help.htm#csh-eu-quick-access-apps'
-    });
-    expect(result).toEqual('help.foo.com/okta_help.htm#cshid=csh-eu-quick-access-apps');
-  });
-});
+import { getRedirectUrl, getUrlParameter } from '../../src/UrlUtil';
 
 describe('UrlUtil.getRedirectUrl', () => {
-  const setup = (hash) => {
+  const setup = (href) => {
     // reset values
     jest.clearAllMocks();
     let windowSpy = jest.spyOn(window, 'window', 'get');
     windowSpy.mockImplementation(() => {
       return {
         location: {
-          hash,
+          href,
         }
       };
     });
   };
   test('generates redirect url correctly for admin docs with locale query param', () => {
-    setup('cshid=Settings_Emails_SMS?locale=ja');
-    const result = getRedirectUrl({
-      locale: 'ja',
-      aliasUrl: 'help.foo.com/okta_help.htm#cshid=Settings_Emails_SMS?locale=ja'
-    });
-    expect(result).toEqual('help.foo.com/ja/prod/okta_help.htm#cshid=Settings_Emails_SMS');
+    setup('help.foo.com/okta_help.htm?id=csh-user-ov-overview&locale=ja');
+    const result = getRedirectUrl();
+    expect(result).toEqual('help.foo.com/en/prod/okta_help.htm#cshid=csh-user-ov-overview');
   });
 
   test('generates redirect url correctly for admin docs without locale query param', () => {
-    setup('cshid=Settings_Emails_SMS');
-    const result = getRedirectUrl({
-      locale: 'en',
-      aliasUrl: 'help.foo.com/okta_help.htm#cshid=Settings_Emails_SMS'
-    });
-    expect(result).toEqual('help.foo.com/en/prod/okta_help.htm#cshid=Settings_Emails_SMS');
+    setup('help.foo.com/okta_help.htm?id=csh-user-ov-overview');
+    const result = getRedirectUrl();
+    expect(result).toEqual('help.foo.com/en/prod/okta_help.htm#cshid=csh-user-ov-overview');
   });
 
   test('generates redirect url correctly for end-user docs with locale query param', () => {
-    setup('cshid=Settings_Emails_SMS?locale=ja&type=end-user');
-    const result = getRedirectUrl({
-      locale: 'ja-jp',
-      aliasUrl: 'help.foo.com/okta_help.htm#cshid=Settings_Emails_SMS?locale=ja&type=end-user'
-    });
-    expect(result).toEqual('help.foo.com/end-user/ja-jp/okta_help.htm#cshid=Settings_Emails_SMS');
+    setup('help.foo.com/okta_help.htm?id=csh-user-ov-overview&locale=ja&type=end-user');
+    const result = getRedirectUrl();
+    expect(result).toEqual('help.foo.com/end-user/ja-jp/okta_help.htm#cshid=csh-user-ov-overview');
   });
 
   test('generates redirect url correctly for end-user docs without locale query param', () => {
-    setup('cshid=Settings_Emails_SMS?type=end-user');
-    const result = getRedirectUrl({
-      locale: 'ja-jp',
-      aliasUrl: 'help.foo.com/okta_help.htm#cshid=Settings_Emails_SMS?type=end-user'
-    });
-    expect(result).toEqual('help.foo.com/end-user/ja-jp/okta_help.htm#cshid=Settings_Emails_SMS');
+    setup('help.foo.com/okta_help.htm?id=csh-user-ov-overview&type=end-user&locale=en');
+    const result = getRedirectUrl();
+    expect(result).toEqual('help.foo.com/end-user/en-us/okta_help.htm#cshid=csh-user-ov-overview');
   });
 
 });
 
 describe('UrlUtil.getUrlParameter', () => {
-  const setup = (hash) => {
+  const setup = (href) => {
     // reset values
     jest.clearAllMocks();
     let windowSpy = jest.spyOn(window, 'window', 'get');
     windowSpy.mockImplementation(() => {
       return {
         location: {
-          hash,
+          href,
         }
       };
     });
   };
   test('extracts locale query param correctly', () => {
-    setup('cshid=Settings_Emails_SMS?locale=ja-JP&type=end-user');
+    setup('cshid=Settings_Emails_SMS&locale=ja-JP&type=end-user');
     const result = getUrlParameter('locale');
     expect(result).toEqual('ja-JP');
   });
 
   test('extracts type query param correctly', () => {
-    setup('cshid=Settings_Emails_SMS?type=end-user&locale=en-US');
+    setup('cshid=Settings_Emails_SMS&type=end-user&locale=en-US');
     const result = getUrlParameter('type');
     expect(result).toEqual('end-user');
   });
