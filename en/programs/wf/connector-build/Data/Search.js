3f7b088bf07249dcacabb71f7b2be70f7a584de9
Mark Moussa <52501392+markmoussa-okta@users.noreply.github.com>
2021-03-19 16:44:46 -0400

    Revert "build for connector builder v2"
    This reverts commit eebd9499cc895b569e6b53f2cb2d88885841b131.

--- en/programs/wf/connector-build/Data/Search.js
+++
@@ -1,1 +0,0 @@
-define({r:1,st:'Stem',n:1,t:[0],m:[],u:['../Content/Topics/betas/closed/connector-builder/about-connections.htm'],s:['+'],p:[['+',4]]});
--- en/programs/wf/connector-build/Resources/Scripts/require.config.js
+++
@@ -1,3 +0,0 @@
-require.config({
-    urlArgs: 't=637517682700037212'
-});
