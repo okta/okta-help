eebd9499cc895b569e6b53f2cb2d88885841b131
Mark Moussa <52501392+markmoussa-okta@users.noreply.github.com>
2021-03-19 16:32:39 -0400

    build for connector builder v2

---
+++ en/programs/wf/connector-build/Data/Alias.js
@@ -0,0 +1,6 @@
+var xmlAliasData = "";
+xmlAliasData += '<?xml version=\"1.0\" encoding=\"utf-8\"?>';
+xmlAliasData += '<CatapultAliasFile>';
+xmlAliasData += '    <Map Name=\"ext-connector-builder\" Link=\"Topics/betas/closed/connector-builder/connector-builder.htm\" ResolvedId=\"30304\" />';
+xmlAliasData += '</CatapultAliasFile>';
+MadCap.Utilities.Xhr._FilePathToXmlStringMap.Add('Alias', xmlAliasData);
---
+++ en/programs/wf/connector-build/Resources/Scripts/require.config.js
@@ -0,0 +1,3 @@
+require.config({
+    urlArgs: 't=637517682700037212'
+});
