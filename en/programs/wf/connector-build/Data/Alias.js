1880e050d3fc95737f114695507743699f4297ba
markcowan <52024619+MarkCowan-okta@users.noreply.github.com>
2021-04-28 18:33:02 -0600

    Updates from MM for connector builder

---
+++ en/programs/wf/connector-build/Data/Alias.js
@@ -0,0 +1,8 @@
+var xmlAliasData = "";
+xmlAliasData += '<?xml version=\"1.0\" encoding=\"utf-8\"?>';
+xmlAliasData += '<CatapultAliasFile>';
+xmlAliasData += '    <Map Name=\"ext-connector-builder\" Link=\"Topics/betas/closed/connector-builder/connector-builder.htm\" ResolvedId=\"30304\" />';
+xmlAliasData += '    <Map Name=\"ext-cbv2-plan-limitations\" Link=\"Topics/betas/closed/connector-builder/developer-plan-limits.htm\" ResolvedId=\"31042\" />';
+xmlAliasData += '    <Map Name=\"ext-dev-plan-limits\" Link=\"Topics/betas/closed/connector-builder/developer-plan-limits.htm\" ResolvedId=\"31043\" />';
+xmlAliasData += '</CatapultAliasFile>';
+MadCap.Utilities.Xhr._FilePathToXmlStringMap.Add('Alias', xmlAliasData);
---
+++ en/programs/wf/connector-build/Data/Search.js
@@ -0,0 +1,1 @@
+define({r:1,st:'Stem',n:1,t:[0],m:[],u:['../Content/Topics/betas/closed/connector-builder/about-building-blocks.htm'],s:['+'],p:[['+',5]]});
