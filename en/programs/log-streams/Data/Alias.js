baa810fe06094355dacf2e5d2f806cd656a2af75
paulwallace-okta <paul.wallace@okta.com>
2021-10-26 17:16:18 -0400

    Updating log streams build

--- en/programs/log-streams/Data/Alias.js
+++ en/programs/log-streams/Data/Alias.js
@@ -1,5 +1,6 @@
 var xmlAliasData = "";
 xmlAliasData += '<?xml version=\"1.0\" encoding=\"utf-8\"?>';
 xmlAliasData += '<CatapultAliasFile>';
+xmlAliasData += '    <Map Name=\"csh-log-streams\" Link=\"Topics/betas/closed/log-streams/about-log-streams.htm\" ResolvedId=\"13005\" />';
 xmlAliasData += '</CatapultAliasFile>';
 MadCap.Utilities.Xhr._FilePathToXmlStringMap.Add('Alias', xmlAliasData);
--- en/programs/log-streams/Resources/Scripts/require.config.js
+++ en/programs/log-streams/Resources/Scripts/require.config.js
@@ -1,3 +1,7 @@
 require.config({
+<<<<<<< Updated upstream
     urlArgs: 't=637697310262603899'
+=======
+    urlArgs: 't=637708638537522749'
+>>>>>>> Stashed changes
 });
