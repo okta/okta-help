18f80f18ebe9d2728f0bdd44ed0fd86dc15d3de1
nikitaapraj-okta <nikita.apraj@okta.com>
2021-10-27 14:34:37 -0700

    OIE RN 2021.10.2

--- oie/en-us/Data/Concepts.js
+++ oie/en-us/Data/Concepts.js
@@ -1,1 +1,1 @@
-define({'ad-agent':[927,54,56],'dir-intg':[34],});
+define({'ad-agent':[928,54,56],'dir-intg':[34],});
--- oie/en-us/Resources/Scripts/require.config.js
+++ oie/en-us/Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=637707797705211376'
+    urlArgs: 't=637709405488649244'
 });
