da9a50820c1827464bf3769ec1092faa0b704055
markcowan <52024619+MarkCowan-okta@users.noreply.github.com>
2021-12-13 09:28:33 -0700

    new classic en-us help site for testing

---
+++ en-us/Data/Search.js
@@ -0,0 +1,1 @@
+define({r:1,st:'Stem',n:1,t:[0],m:[],u:['../Content/Topics/Access-Gateway/oag-home.htm'],s:['$'],p:[['$',3]]});
---
+++ en-us/Resources/Scripts/require.config.js
@@ -0,0 +1,3 @@
+require.config({
+    urlArgs: 't=637749825926119657'
+});
