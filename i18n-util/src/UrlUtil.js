f97076f1869cc1a68e7cc9ddb05892fe59b76240
Paul Wallace <paul.wallace@okta.com>
2022-02-27 18:48:02 -0800

    Revert "Merge pull request #57 from okta/pwallace-rollback-OKTA-453993"
    This reverts commit ae70c07a75e3c3654fd78352b48ab4d69a58e0ca, reversing
    changes made to edd7b25b8724ba16f92330f7cb37b233e7e69c7c.

--- i18n-util/src/UrlUtil.js
+++ i18n-util/src/UrlUtil.js
@@ -16,14 +16,15 @@ const getRedirectUrl = () =>  {
   // Currently type is only used for end-user docs. In the future it will be used for admin docs
   const type = getUrlParameter('type');
   const id = getUrlParameter('id');
-  /** 
-   * env is only added to support the current version of admin docs that dont have any translations
-   * and are under en/prod folder. https://github.com/okta/okta-help/tree/gh-pages/en/prod
+  /**
+   * "host": `https://help.okta.com`
+   * "type": `oie` | `wf` | `asa` | `eu` | `oag`
+   *    "type" is not provided for links to "Classic" engine content
+   * "locale": `en-us` | `ja-jp`
    * **/
-  const env = 'prod';
   const redirectUrl = type ?
     `${host}/${type}/${locale}/${fileName}#cshid=${id}`:
-    `${host}/${locale}/${env}/${fileName}#cshid=${id}`;
+    `${host}/${locale}/${fileName}#cshid=${id}`;
   return redirectUrl;
 
 };
@@ -31,4 +32,4 @@ const getRedirectUrl = () =>  {
 export {
   getRedirectUrl,
   getUrlParameter
-}; 
+};
