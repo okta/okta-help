f17cb1f4f4f046fc7a4d56b57515aaa98414983f
markcowan <52024619+MarkCowan-okta@users.noreply.github.com>
2022-03-14 16:58:34 -0600

    try csi beta  again

---
+++ en/programs/csi/Data/Alias.js
@@ -0,0 +1,7 @@
+var xmlAliasData = "";
+xmlAliasData += '<?xml version=\"1.0\" encoding=\"utf-8\"?>';
+xmlAliasData += '<CatapultAliasFile>';
+xmlAliasData += '    <Map Name=\"csh-csi-create-custom-inline-hook\" Link=\"Topics/betas/closed/csi/csi-create-custom-inline-hook.htm\" ResolvedId=\"15024\" />';
+xmlAliasData += '    <Map Name=\"csh-csi-main\" Link=\"Topics/betas/closed/csi/csi-main.htm\" ResolvedId=\"15025\" />';
+xmlAliasData += '</CatapultAliasFile>';
+MadCap.Utilities.Xhr._FilePathToXmlStringMap.Add('Alias', xmlAliasData);
---
+++ en/programs/csi/Data/Search.js
@@ -0,0 +1,1 @@
+define({r:1,st:'Stem',n:1,t:[0],m:[],u:['../Content/Topics/betas/closed/csi/csi-create-custom-idp.htm'],s:['1'],p:[['1',5]]});
