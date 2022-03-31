27f4e944e9839ccd76d472b77a2d6fb245cb800e
damianali-okta <damian.ali@okta.com>
2022-03-31 10:53:11 -0400

    Open beta build for Event Hooks

--- en/betas/Data/Alias.js
+++ en/betas/Data/Alias.js
@@ -1,7 +1,8 @@
 var xmlAliasData = "";
 xmlAliasData += '<?xml version=\"1.0\" encoding=\"utf-8\"?>';
 xmlAliasData += '<CatapultAliasFile>';
-xmlAliasData += '    <Map Name=\"ext-passwordless\" Link=\"Topics/password-optional/password-optional-disabled.htm\" ResolvedId=\"1514\" />';
-xmlAliasData += '    <Map Name=\"csh-passwordless\" Link=\"Topics/password-optional/password-optional-disabled.htm\" ResolvedId=\"1515\" />';
+xmlAliasData += '    <Map Name=\"ext-about-eventhooks-el\" Link=\"Topics/betas/open/event-hooks/about-eventhooks-el.htm\" ResolvedId=\"33076\" />';
+xmlAliasData += '    <Map Name=\"ad_agent\" Link=\"Topics/betas/open/event-hooks/about-eventhooks-el.htm\" ResolvedId=\"3073\" />';
+xmlAliasData += '    <Map Name=\"ext-eventhooks-main\" Link=\"Topics/betas/open/event-hooks/eventhooks-main.htm\" ResolvedId=\"33096\" />';
 xmlAliasData += '</CatapultAliasFile>';
 MadCap.Utilities.Xhr._FilePathToXmlStringMap.Add('Alias', xmlAliasData);
--- en/betas/Resources/Scripts/require.config.js
+++ en/betas/Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=637817658527687393'
+    urlArgs: 't=637843206355673518'
 });
