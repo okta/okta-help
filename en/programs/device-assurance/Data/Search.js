61ef552dcc87ebc4a20da33eaf05b413c1847448
Ian Hakes <ian.hakes@okta.com>
2022-06-29 12:56:58 -0400

    Updates Device Assurance [closed beta] docs
    Resolves OKTA-510572

--- en/programs/device-assurance/Data/Search.js
+++ en/programs/device-assurance/Data/Search.js
@@ -1,1 +1,1 @@
-define({r:1,st:'Stem',n:1,t:[0],m:[],u:['../Content/Topics/betas/closed/device-assurance/device-assurance-add-user-help.htm'],s:['0'],p:[['0',7]]});
+define({r:1,st:'Stem',n:1,t:[0],m:[],u:['../Content/Topics/betas/closed/device-assurance/device-assurance-add-user-help.htm'],s:['0'],p:[['0',8]]});
