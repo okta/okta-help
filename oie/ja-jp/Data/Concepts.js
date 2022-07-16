c771d1b8608377c6c8f8f0ba25b5badb8b137381
Paul Wallace <paul.wallace@okta.com>
2022-07-15 17:02:14 -0700

    Updating OIE content

--- oie/ja-jp/Data/Concepts.js
+++ oie/ja-jp/Data/Concepts.js
@@ -1,1 +1,1 @@
-define({'ad-agent':[54,56,860],'dir-intg':[34],});
+define({'ad-agent':[54,860,56],'dir-intg':[34],});
--- oie/ja-jp/Resources/Scripts/require.config.js
+++ oie/ja-jp/Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=637928883686564993'
+    urlArgs: 't=637935001940928254'
 });
