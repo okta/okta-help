be84130211e065211200509eb040117e61bcd895
paulwallace-okta <paul.wallace@okta.com>
2022-07-27 12:43:35 -0700

    Updating WF content

--- wf/ja-jp/Data/Search.js
+++ wf/ja-jp/Data/Search.js
@@ -1,1 +1,1 @@
-define({r:1,st:'NGram',n:1,t:[0],m:[],u:['../Content/Topics/ReleaseNotes/Workflows/preview.htm'],s:[' '],p:[[' ',1]]});
+define({r:1,st:'NGram',n:1,t:[0],m:[],u:['../Content/Topics/ReleaseNotes/Workflows/preview.htm'],s:[' '],p:[[' ',0]]});
