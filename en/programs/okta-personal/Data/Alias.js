b49c3310d788be3bcdcab75e81266b15debb51f8
Ian Hakes <ian.hakes@okta.com>
2022-08-18 10:37:12 -0400

    Adds Okta Personal closed beta docs
    Resolves OKTA-524402

---
+++ en/programs/okta-personal/Data/Alias.js
@@ -0,0 +1,6 @@
+var xmlAliasData = "";
+xmlAliasData += '<?xml version=\"1.0\" encoding=\"utf-8\"?>';
+xmlAliasData += '<CatapultAliasFile>';
+xmlAliasData += '    <Map Name=\"csh-index\" Link=\"index.htm\" ResolvedId=\"1298\" />';
+xmlAliasData += '</CatapultAliasFile>';
+MadCap.Utilities.Xhr._FilePathToXmlStringMap.Add('Alias', xmlAliasData);
