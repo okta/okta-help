57823002bede849de9c7dbc2bcf7a490248233df
Paul Wallace <paul.wallace@okta.com>
2022-09-26 15:00:16 -0700

    Updating Classic content (#459)

--- en-us/Data/Alias.js
+++ en-us/Data/Alias.js
@@ -615,5 +615,6 @@ xmlAliasData += '    <Map Name=\"ext-about-behavior-detection\" Link=\"Topics/Se
 xmlAliasData += '    <Map Name=\"csh-ar-create-req-type\" Link=\"Topics/identity-governance/access-requests/ar-create-request-type.htm\" ResolvedId=\"13018\" />';
 xmlAliasData += '    <Map Name=\"ext-view-import-monitoring-dashboard\" Link=\"Topics/users-groups-profiles/usgp-view-import-monitoring-dashboard.htm\" ResolvedId=\"4078\" />';
 xmlAliasData += '    <Map Name=\"csh-session-revoke\" Link=\"Topics/SLO/session-revoke.htm\" ResolvedId=\"14000\" />';
+xmlAliasData += '    <Map Name=\"csh-administrators-admin-comparison\" Link=\"Topics/Security/administrators-admin-comparison.htm\" ResolvedId=\"16013\" />';
 xmlAliasData += '</CatapultAliasFile>';
 MadCap.Utilities.Xhr._FilePathToXmlStringMap.Add('Alias', xmlAliasData);
