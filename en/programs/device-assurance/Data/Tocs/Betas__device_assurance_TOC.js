e86d21526de19a013052038731f3b5247fef2e3d
Ian Hakes <ian.hakes@okta.com>
2022-09-27 13:27:10 -0400

    Removes device assurance beta content from h.o.c

--- en/programs/device-assurance/Data/Tocs/Betas__device_assurance_TOC.js
+++
@@ -1,1 +0,0 @@
-define({numchunks:1,prefix:'Betas__device_assurance_TOC_Chunk',chunkstart:['/Content/Topics/betas/closed/device-assurance/device-assurance-add-user-help.htm'],tree:{n:[{i:0,c:0,n:[{i:1,c:0},{i:2,c:0},{i:3,c:0},{i:4,c:0},{i:5,c:0},{i:6,c:0},{i:7,c:0,n:[{i:8,c:0},{i:9,c:0},{i:10,c:0},{i:11,c:0},{i:12,c:0}]}]}]}});
