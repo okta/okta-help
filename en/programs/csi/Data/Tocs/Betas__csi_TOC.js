f5836e84d2ac181420075255950c2d4e1cbd73ca
Ian Hakes <ian.hakes@okta.com>
2022-09-27 13:28:08 -0400

    Removes csi beta content from h.o.c

--- en/programs/csi/Data/Tocs/Betas__csi_TOC.js
+++
@@ -1,1 +0,0 @@
-define({numchunks:1,prefix:'Betas__csi_TOC_Chunk',chunkstart:['/Content/Topics/betas/closed/csi/csi-create-custom-idp.htm'],tree:{n:[{i:0,c:0,n:[{i:1,c:0},{i:2,c:0},{i:3,c:0},{i:4,c:0},{i:5,c:0}]}]}});
