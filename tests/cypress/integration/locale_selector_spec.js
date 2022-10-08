4ef8d3e7d9f581834509e80c1783b3e772d1559d
Paul Wallace <paul.wallace@okta.com>
2022-10-07 20:17:03 -0700

    Refactoring locale switcher tests

--- tests/cypress/integration/locale_selector_spec.js
+++ tests/cypress/integration/locale_selector_spec.js
@@ -1,34 +1,306 @@
-describe('Locale is set to ja-JP', () => {
+describe('Locale switching for H.O.C. home page', () => {
 
-  it('when Japanese is selected', () => {
+  it('switches from en-US to ja-JP', () => {
     cy.visit('en-us/Content/index.htm')
-    // change locale to ja-JP
-    cy.get('[title="Change language"]')
-      .should('be.hidden')
-      .invoke('show')
+    cy.get('button.select-language-button')
+      .trigger('mouseover')
+      .find('div.button-icon-wrapper')
+      .then(($el) => {
+        cy.wrap($el)
+          .should('have.attr', 'aria-label', 'Change language')
+          .should('be.visible')
+      })
       .click()
     cy.get('a').contains('日本語 (日本)‎')
       .should('be.visible')
       .click()
     cy.url().should('include', 'ja-jp/Content/index.htm')
   })
-})
 
-
-describe('Locale is set to en-US', () => {
-
-  it('when English is selected', () => {
+  it('switches from ja-JP to en-US', () => {
     cy.visit('ja-jp/Content/index.htm')
-    // change locale to en-US
-    // 'title' should be "言語の変更" but isn't, this is a bug in Flare's JS
-    cy.get('[title="Change language"]')
-      .should('be.hidden')
-      .invoke('show')
+    cy.get('button.select-language-button')
+      .trigger('mouseover')
+      .find('div.button-icon-wrapper')
+      .then(($el) => {
+        cy.wrap($el)
+          // 'title' should be "言語の変更" but isn't -- we need to translate aria-labels!
+          .should('have.attr', 'aria-label', 'Change language')
+          .should('be.visible')
+      })
       .click()
     cy.get('a')
       .contains('English (United States)‎')
       .should('be.visible')
       .click()
     cy.url().should('include', 'en-us/Content/index.htm')
+  })
+})
+
+
+describe('Locale switching for ASA', () => {
+
+  it('switches from en-US to ja-JP', () => {
+    cy.visit('asa/en-us/Content/Topics/Adv_Server_Access/docs/asa-overview.htm')
+    cy.get('button.select-language-button')
+      .trigger('mouseover')
+      .find('div.button-icon-wrapper')
+      .then(($el) => {
+        cy.wrap($el)
+          .should('have.attr', 'aria-label', 'Change language')
+          .should('be.visible')
+      })
+      .click()
+    cy.get('a').contains('日本語 (日本)‎')
+      .should('be.visible')
+      .click()
+    cy.url().should('include', 'asa/ja-jp/Content/Topics/Adv_Server_Access/docs/asa-overview.htm')
+  })
+
+  it('switches from ja-JP to en-US', () => {
+    cy.visit('asa/ja-jp/Content/Topics/Adv_Server_Access/docs/asa-overview.htm')
+    cy.get('button.select-language-button')
+      .trigger('mouseover')
+      .find('div.button-icon-wrapper')
+      .then(($el) => {
+        cy.wrap($el)
+          .should('have.attr', 'aria-label', '言語の変更')
+          .should('be.visible')
+      })
+      .click()
+    cy.get('a')
+      .contains('English (United States)‎')
+      .should('be.visible')
+      .click()
+    cy.url().should('include', 'asa/en-us/Content/Topics/Adv_Server_Access/docs/asa-overview.htm')
+  })
+
+})
+
+describe('Locale switching for Classic', () => {
+
+  it('switches from en-US to ja-JP', () => {
+    cy.visit('en-us/Content/index-admin.htm')
+    cy.get('button.select-language-button')
+      .trigger('mouseover')
+      .find('div.button-icon-wrapper')
+      .then(($el) => {
+        cy.wrap($el)
+          .should('have.attr', 'aria-label', 'Change language')
+          .should('be.visible')
+      })
+      .click()
+    cy.get('a').contains('日本語 (日本)‎')
+      .should('be.visible')
+      .click()
+    cy.url().should('include', 'ja-jp/Content/index-admin.htm')
+  })
+
+  it('switches from ja-JP to en-US', () => {
+    cy.visit('ja-jp/Content/index-admin.htm')
+    cy.get('button.select-language-button')
+      .trigger('mouseover')
+      .find('div.button-icon-wrapper')
+      .then(($el) => {
+        cy.wrap($el)
+          .should('have.attr', 'aria-label', '言語の変更')
+          .should('be.visible')
+      })
+      .click()
+    cy.get('a')
+      .contains('English (United States)‎')
+      .should('be.visible')
+      .click()
+    cy.url().should('include', 'en-us/Content/index-admin.htm')
+  })
+})
+
+describe('Locale switching for Classic release notes', () => {
+
+  it('switches from en-US to ja-JP', () => {
+    cy.visit('en-us/Content/Topics/ReleaseNotes/okta-relnotes.htm')
+    cy.get('button.select-language-button')
+      .trigger('mouseover')
+      .find('div.button-icon-wrapper')
+      .then(($el) => {
+        cy.wrap($el)
+          .should('have.attr', 'aria-label', 'Change language')
+          .should('be.visible')
+      })
+      .click()
+    cy.get('a').contains('日本語 (日本)‎')
+      .should('be.visible')
+      .click()
+    cy.url().should('include', 'ja-jp/Content/Topics/ReleaseNotes/okta-relnotes.htm')
+  })
+
+  it('switches from ja-JP to en-US', () => {
+    cy.visit('ja-jp/Content/Topics/ReleaseNotes/okta-relnotes.htm')
+    cy.get('button.select-language-button')
+      .trigger('mouseover')
+      .find('div.button-icon-wrapper')
+      .then(($el) => {
+        cy.wrap($el)
+          .should('have.attr', 'aria-label', '言語の変更')
+          .should('be.visible')
+      })
+      .click()
+    cy.get('a')
+      .contains('English (United States)‎')
+      .should('be.visible')
+      .click()
+    cy.url().should('include', 'en-us/Content/Topics/ReleaseNotes/okta-relnotes.htm')
+  })
+})
+
+describe('Locale switching for End User', () => {
+
+  it('switches from en-US to ja-JP', () => {
+    cy.visit('eu/en-us/Content/Topics/end-user/end-user-home.htm')
+    cy.get('button.select-language-button')
+      .trigger('mouseover')
+      .find('div.button-icon-wrapper')
+      .then(($el) => {
+        cy.wrap($el)
+          .should('have.attr', 'aria-label', 'Change language')
+          .should('be.visible')
+      })
+      .click()
+    cy.get('a').contains('日本語 (日本)‎')
+      .should('be.visible')
+      .click()
+    cy.url().should('include', 'eu/ja-jp/Content/Topics/end-user/end-user-home.htm')
+  })
+
+  it('switches from ja-JP to en-US', () => {
+    cy.visit('eu/ja-jp/Content/Topics/end-user/end-user-home.htm')
+    cy.get('button.select-language-button')
+      .trigger('mouseover')
+      .find('div.button-icon-wrapper')
+      .then(($el) => {
+        cy.wrap($el)
+          .should('have.attr', 'aria-label', '言語の変更')
+          .should('be.visible')
+      })
+      .click()
+    cy.get('a')
+      .contains('English (United States)‎')
+      .should('be.visible')
+      .click()
+    cy.url().should('include', 'eu/en-us/Content/Topics/end-user/end-user-home.htm')
+  })
+})
+
+describe('Locale switching for OIE', () => {
+
+  it('switches from en-US to ja-JP', () => {
+    cy.visit('oie/en-us/Content/Topics/identity-engine/oie-index.htm')
+    cy.get('button.select-language-button')
+      .trigger('mouseover')
+      .find('div.button-icon-wrapper')
+      .then(($el) => {
+        cy.wrap($el)
+          .should('have.attr', 'aria-label', 'Change language')
+          .should('be.visible')
+      })
+      .click()
+    cy.get('a').contains('日本語 (日本)‎')
+      .should('be.visible')
+      .click()
+    cy.url().should('include', 'oie/ja-jp/Content/Topics/identity-engine/oie-index.htm')
+  })
+
+  it('switches from ja-JP to en-US', () => {
+    cy.visit('oie/ja-jp/Content/Topics/identity-engine/oie-index.htm')
+    cy.get('button.select-language-button')
+      .trigger('mouseover')
+      .find('div.button-icon-wrapper')
+      .then(($el) => {
+        cy.wrap($el)
+          .should('have.attr', 'aria-label', '言語の変更')
+          .should('be.visible')
+      })
+      .click()
+    cy.get('a')
+      .contains('English (United States)‎')
+      .should('be.visible')
+      .click()
+    cy.url().should('include', 'oie/en-us/Content/Topics/identity-engine/oie-index.htm')
+  })
+})
+
+describe('Locale switching for OIE release notes', () => {
+
+  it('switches from en-US to ja-JP', () => {
+    cy.visit('oie/en-us/Content/Topics/ReleaseNotes/oie-relnotes.htm')
+    cy.get('button.select-language-button')
+      .trigger('mouseover')
+      .find('div.button-icon-wrapper')
+      .then(($el) => {
+        cy.wrap($el)
+          .should('have.attr', 'aria-label', 'Change language')
+          .should('be.visible')
+      })
+      .click()
+    cy.get('a').contains('日本語 (日本)‎')
+      .should('be.visible')
+      .click()
+    cy.url().should('include', 'oie/ja-jp/Content/Topics/ReleaseNotes/oie-relnotes.htm')
+  })
+
+  it('switches from ja-JP to en-US', () => {
+    cy.visit('oie/ja-jp/Content/Topics/ReleaseNotes/oie-relnotes.htm')
+    cy.get('button.select-language-button')
+      .trigger('mouseover')
+      .find('div.button-icon-wrapper')
+      .then(($el) => {
+        cy.wrap($el)
+          .should('have.attr', 'aria-label', '言語の変更')
+          .should('be.visible')
+      })
+      .click()
+    cy.get('a')
+      .contains('English (United States)‎')
+      .should('be.visible')
+      .click()
+    cy.url().should('include', 'oie/en-us/Content/Topics/ReleaseNotes/oie-relnotes.htm')
+  })
+})
+
+describe('Locale switching for Workflows', () => {
+
+  it('switches from en-US to ja-JP', () => {
+    cy.visit('wf/en-us/Content/Topics/Workflows/workflows-main.htm')
+    cy.get('button.select-language-button')
+      .trigger('mouseover')
+      .find('div.button-icon-wrapper')
+      .then(($el) => {
+        cy.wrap($el)
+          .should('have.attr', 'aria-label', 'Change language')
+          .should('be.visible')
+      })
+      .click()
+    cy.get('a').contains('日本語 (日本)‎')
+      .should('be.visible')
+      .click()
+    cy.url().should('include', 'wf/ja-jp/Content/Topics/Workflows/workflows-main.htm')
+  })
+
+  it('switches from ja-JP to en-US', () => {
+    cy.visit('wf/ja-jp/Content/Topics/Workflows/workflows-main.htm')
+    cy.get('button.select-language-button')
+      .trigger('mouseover')
+      .find('div.button-icon-wrapper')
+      .then(($el) => {
+        cy.wrap($el)
+          .should('have.attr', 'aria-label', '言語の変更')
+          .should('be.visible')
+      })
+      .click()
+    cy.get('a')
+      .contains('English (United States)‎')
+      .should('be.visible')
+      .click()
+    cy.url().should('include', 'wf/en-us/Content/Topics/Workflows/workflows-main.htm')
   })
 })
