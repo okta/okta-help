cb6c064bc8b2a5a014762455cbf87d61dfb24122
emikhasyak-okta <emikhasyak@okta.com>
2022-10-11 14:53:55 -0400

    6

--- scripts/test-pr.sh
+++ scripts/test-pr.sh
@@ -1,5 +1,26 @@
+#!/bin/bash
+set -x
+
 echo Hello 2
 
 cd ..
 
 ls
+
+git status
+
+
+git checkout -b test-commit
+git push origin test-commit
+
+echo 'test change' > data.txt
+
+git status
+git add --all
+
+git commit -m "test commit message"
+git push origin test-commit
+
+gh pr create --title "test PR" --body "test 123" --base em-test-bacon-task
+
+echo Does it work?
