4b859c31a28c6c558f7908364d85c759f75e16f3
emikhasyak-okta <emikhasyak@okta.com>
2022-10-11 15:27:14 -0400

    8

--- scripts/test-pr.sh
+++ scripts/test-pr.sh
@@ -7,21 +7,21 @@ cd ..
 
 ls
 
-brew install gh
+npm install gh
 
 git status
 
 
-git checkout -b test-commit1
-git push origin test-commit1
+git checkout -b test-commit2
+git push origin test-commit2
 
 echo 'test change' > data.txt
 
 git status
 git add --all
 
-git commit --author="Name <>" -m "test commit message"
-git push origin test-commit1
+git -c user.name='Eugene' -c user.email=emikhasyak@okta.com commit -m "test commit message"
+git push origin test-commit2
 
 gh pr create --title "test PR" --body "test 123" --base em-test-bacon-task
 
