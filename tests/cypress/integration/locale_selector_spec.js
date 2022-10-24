69bb9274d13b3d38b36fe46281bccc7cb1f63ace
Paul Wallace <paul.wallace@okta.com>
2022-10-24 16:52:03 -0700

    Wordsmithing

--- tests/cypress/integration/locale_selector_spec.js
+++ tests/cypress/integration/locale_selector_spec.js
@@ -1,6 +1,6 @@
 describe('Locale switching', () => {
 
-  it('routes to expected URLs for H.O.C. home', () => {
+  it('redirects to expected URLs for H.O.C. home', () => {
     cy.visit('en-us/Content/index.htm')
     cy.switchLocale('Change language', '日本語 (日本)‎')
     cy.url().should('include', 'ja-jp/Content/index.htm')
@@ -8,7 +8,7 @@ describe('Locale switching', () => {
     cy.url().should('include', 'en-us/Content/index.htm')
   })
 
-  it('routes to expected URLs for ASA', () => {
+  it('redirects to expected URLs for ASA', () => {
     cy.visit('asa/en-us/Content/Topics/Adv_Server_Access/docs/asa-overview.htm')
     cy.switchLocale('Change language', '日本語 (日本)‎')
     cy.url().should('include', 'asa/ja-jp/Content/Topics/Adv_Server_Access/docs/asa-overview.htm')
@@ -16,7 +16,7 @@ describe('Locale switching', () => {
     cy.url().should('include', 'asa/en-us/Content/Topics/Adv_Server_Access/docs/asa-overview.htm')
   })
 
-  it('routes to expected URLs for Classic', () => {
+  it('redirects to expected URLs for Classic', () => {
     cy.visit('en-us/Content/index-admin.htm')
     cy.switchLocale('Change language', '日本語 (日本)‎')
     cy.url().should('include', 'ja-jp/Content/index-admin.htm')
@@ -24,7 +24,7 @@ describe('Locale switching', () => {
     cy.url().should('include', 'en-us/Content/index-admin.htm')
   })
 
-  it('routes to expected URLs for Classic release notes', () => {
+  it('redirects to expected URLs for Classic release notes', () => {
     cy.visit('en-us/Content/Topics/ReleaseNotes/okta-relnotes.htm')
     cy.switchLocale('Change language', '日本語 (日本)‎')
     cy.url().should('include', 'ja-jp/Content/Topics/ReleaseNotes/okta-relnotes.htm')
@@ -32,7 +32,7 @@ describe('Locale switching', () => {
     cy.url().should('include', 'en-us/Content/Topics/ReleaseNotes/okta-relnotes.htm')
   })
 
-  it('routes to expected URLs for End User', () => {
+  it('redirects to expected URLs for End User', () => {
     cy.visit('eu/en-us/Content/Topics/end-user/end-user-home.htm')
     cy.switchLocale('Change language', '日本語 (日本)‎')
     cy.url().should('include', 'eu/ja-jp/Content/Topics/end-user/end-user-home.htm')
@@ -40,7 +40,7 @@ describe('Locale switching', () => {
     cy.url().should('include', 'eu/en-us/Content/Topics/end-user/end-user-home.htm')
   })
 
-  it('routes to expected URLs for OIE', () => {
+  it('redirects to expected URLs for OIE', () => {
     cy.visit('oie/en-us/Content/Topics/identity-engine/oie-index.htm')
     cy.switchLocale('Change language', '日本語 (日本)‎')
     cy.url().should('include', 'oie/ja-jp/Content/Topics/identity-engine/oie-index.htm')
@@ -48,7 +48,7 @@ describe('Locale switching', () => {
     cy.url().should('include', 'oie/en-us/Content/Topics/identity-engine/oie-index.htm')
   })
 
-  it('routes to expected URLs for OIE release notes', () => {
+  it('redirects to expected URLs for OIE release notes', () => {
     cy.visit('oie/en-us/Content/Topics/ReleaseNotes/oie-relnotes.htm')
     cy.switchLocale('Change language', '日本語 (日本)‎')
     cy.url().should('include', 'oie/ja-jp/Content/Topics/ReleaseNotes/oie-relnotes.htm')
@@ -56,7 +56,7 @@ describe('Locale switching', () => {
     cy.url().should('include', 'oie/en-us/Content/Topics/ReleaseNotes/oie-relnotes.htm')
   })
 
-  it('routes to expected URLs for Workflows', () => {
+  it('redirects to expected URLs for Workflows', () => {
     cy.visit('wf/en-us/Content/Topics/Workflows/workflows-main.htm')
     cy.switchLocale('Change language', '日本語 (日本)‎')
     cy.url().should('include', 'wf/ja-jp/Content/Topics/Workflows/workflows-main.htm')
