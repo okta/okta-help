d615dae1b37485889ac15f55fc16c2a6ac0056dd
emikhasyak-okta <emikhasyak@okta.com>
2022-10-25 08:40:38 -0400

    Temporary adds build script to the master branch (#521)
    OKTA-544377 Temporary adds build script to the master branch

---
+++ ci-scripts/commit-artifact.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+export ARCHIVE_PATH="target.zip"
+export OUTPUT_FOLDER="../${PUBLISH_DESTINATION}"
+export PRODUCT_NAME="${PUBLISH_DESTINATION:=oce}"
+export TOPIC_BRANCH="build-${PRODUCT_NAME}-$(date +"%Y-%m-%d_%H-%M-%S_%s")"
+
+wget -O ${ARCHIVE_PATH} ${BUILT_ARTIFACT}
+
+tar -xf ${ARCHIVE_PATH} -C ${OUTPUT_FOLDER} --strip-components=1 --overwrite
+
+rm ${ARCHIVE_PATH}
+cd ..
+
+git checkout gh-pages
+git checkout -b ${TOPIC_BRANCH}
+
+git add --all
+git -c user.name='CI Automation' -c user.email=${userEmail} commit -m "Updates ${PRODUCT_NAME}"
+git push origin ${TOPIC_BRANCH}
