90aba9011d3f118e5363b7964fe7f97cfed03d23
Paul Wallace <paul.wallace@okta.com>
2022-10-28 12:55:33 -0700

    Revising scripts

--- scripts/test_translation_postprocessing.py
+++ scripts/test_translation_postprocessing.py
@@ -10,14 +10,14 @@ import translation_postprocessing as tpp
 
 class TestTranslationPostProcessing(unittest.TestCase):
 
-    def test_replace_from_val_with_to_val(self):
+    def test_search_and_replace_replaces_from_str_with_to_str(self):
         text = "I am this close."
         pairs = {"from": "this", "to": "that"}
         expected = "I am that close."
         result = tpp.search_and_replace(text, pairs)
         assert result == expected
 
-    def test_return_same_str_when_no_match(self):
+    def test_search_and_replace_returns_unchanged_str_for_no_match(self):
         text = "I am this close."
         pair = {"from": "here", "to": "eternity"}
         expected = text
@@ -26,6 +26,7 @@ class TestTranslationPostProcessing(unittest.TestCase):
 
     @mock.patch('translation_postprocessing._load_json')
     def test_raise_for_missing_pairs_property(self, mock_load_json):
+        # Using 'couplets' as a prop name in place of 'pairs'
         mock_load_json.return_value = dict(couplets=[{'from': 'here', 'to': 'eternity'}])
         message = "JSON data does not contain expected properties."
         with self.assertRaises(tpp.TranslationPostProcessingException, 
@@ -45,10 +46,10 @@ class TestTranslationPostProcessing(unittest.TestCase):
                 tpp.search_and_replace(text, pair)
             assert str(e.exception) == message
 
-    def test_load_pairs_reads_in_termfile_data(self):
-        """Happy path test."""
+    def test_load_pairs_returns_pairs_from_pairs_file_data(self):
         pairs = tpp.load_pairs('scripts/en2ja.json')
-        assert len(pairs) >= 8
+        # The num of pairs can change over time, thus the GTE assertion
+        assert len(pairs) >= 16
         for pair in pairs:
             assert 'from' in pair.keys()
             assert 'to' in pair.keys()
@@ -69,11 +70,11 @@ class TestTranslationPostProcessing(unittest.TestCase):
             data = tpp.load_pairs(filename)
         assert str(e.exception) == message
 
-    def test_validate_topdir(self):
+    def test_validate_returns_dirname_when_valid(self):
         lang_dir = tpp.validate('asa/ja-jp')
         self.assertEqual(lang_dir, 'asa/ja-jp')
 
-    def test_validate_raises_when_topdir_is_not_a_directory(self):
+    def test_validate_raises_when_lang_dir_is_not_a_directory(self):
         fake_dir = 'here'
         message = "'here' is not a valid directory path."
         with self.assertRaises(tpp.TranslationPostProcessingException,
@@ -81,7 +82,7 @@ class TestTranslationPostProcessing(unittest.TestCase):
             dir_ = tpp.validate(fake_dir)
         assert str(e.exception) == message
 
-    def test_validate_raises_when_topdir_doesnt_point_to_a_lang_locale_dir(self):
+    def test_validate_raises_when_lang_dir_doesnt_point_to_a_lang_locale_dir(self):
         no_locale_dir = 'asa'
         message = "'asa' is not a valid lang-locale directory."
         self.assertTrue(os.path.isdir(no_locale_dir))
--- scripts/translation_postprocessing.py
+++ scripts/translation_postprocessing.py
@@ -5,15 +5,31 @@
 (c) 2022 Okta, Inc.
 Author: Paul Wallace (paul.wallace@okta.com)
 
-Returned translations of HTML content require post-processing prior to 
-deployment.
+Returned translations of HTML content require post-processing prior to
+deployment. There are a number of string segments that are not currently
+picked up in XTM, and thus are returned untranslated. 
+These segments include:
 
-* Attribute values
-* URL fragements for locale-specific paths
-* URL query parameters for language
+* Attribute values (such as aria-labels)
+* URL fragements for locale-specific paths (such as `/en-us/`
+* URL query parameters for language (such as `?language=en_US`) 
 
+This script can be used to execute a search-and-replace of these segments. 
 
+The segments that require fixing are maintained in an accompanying
+JSON file (`en2ja.json`). String replacement pairs can be revised or added 
+therein. If in future we need to support additional languages, we can 
+add similar files for those, which can be sent to translation
+if necessary.
 
+This script uses modules from the Python Standard Library only. It should
+run successfully on any machine that has Python3 installed, without 
+need to fetch additional packages.
+
+This script is accompanied by unit tests. To run the tests:
+
+        $ cd okta-help
+        $ python3 scripts/test_translation_postprocessing.py
 
 """
 
@@ -21,6 +37,8 @@ import argparse
 import json
 import os
 import re
+import subprocess
+import sys
 
 
 # Error messages
@@ -38,36 +56,36 @@ not_a_lang_locale_dir = "'%s' is not a valid lang-locale directory."
 lang_locale_dir = re.compile(r'[a-z]{2}-(?:[a-z]{2}|419)')
 
 
-def validate(topdir):
-    """
-    
-    """
-    if not os.path.isdir(topdir):
-        raise TranslationPostProcessingException(invalid_dir % topdir)
-    base_dir = os.path.basename(topdir)
+def validate(lang_dir):
+    """Returns `lang_pdir` path for valid target lang directories."""
+    if not os.path.isdir(lang_dir):
+        raise TranslationPostProcessingException(invalid_dir % lang_dir)
+    base_dir = os.path.basename(lang_dir)
     if not re.match(lang_locale_dir, base_dir):
-        raise TranslationPostProcessingException(not_a_lang_locale_dir % topdir)
-    return topdir
+        raise TranslationPostProcessingException(not_a_lang_locale_dir % lang_dir)
+    return lang_dir
    
-def load_pairs(termfile):
-    data = _load_json(termfile)
+def load_pairs(pairs_file):
+    """Returns search/replace pairs from pairs_file"""
+    data = _load_json(pairs_file)
     try:
         pairs = data['pairs']
     except KeyError:
         raise TranslationPostProcessingException(missing_properties)
     return pairs
 
-def _load_json(termfile):
-    if not os.path.isfile(termfile):
-        raise TranslationPostProcessingException(invalid_file % termfile)
-    with open(termfile, 'r') as f:
+def _load_json(pairs_file):
+    if not os.path.isfile(pairs_file):
+        raise TranslationPostProcessingException(invalid_file % pairs_file)
+    with open(pairs_file, 'r') as f:
         try:
             data = json.load(f)
         except json.decoder.JSONDecodeError:
-            raise TranslationPostProcessingException(invalid_json % termfile)
+            raise TranslationPostProcessingException(invalid_json % pairs_file)
     return data
 
 def search_and_replace(text, pair):
+    """Returns `text` with any instances of `from` val replaced with `to`."""
     try:
         from_ = pair['from']
         to_ = pair['to']
@@ -77,9 +95,11 @@ def search_and_replace(text, pair):
         text = text.replace(from_, to_)
     return text
 
-
-def apply_replacements(dir, pairs):
-    for root, dirs, files in os.walk(dir):
+def apply_replacements(target_dir, pairs):
+    """Walk the dir tree from `target_dir`, applying string replacements
+       for each pair in `pairs`.
+    """
+    for root, dirs, files in os.walk(target_dir):
         for file in files:
             if file.endswith('.htm'):
                 path = os.path.join(root, file)
@@ -91,14 +111,10 @@ def apply_replacements(dir, pairs):
                     with open(path, 'w') as w:
                         w.write(revised_text)
 
-
-def main(topdir, termfile):
-
-    lang_dir = validate(topdir)
-    replacement_pairs = load_pairs(termfile)
-    apply_replacements(lang_dir, replacement_pairs)
-
-
+def main(lang_dir, pairs_file):
+    target_dir = validate(lang_dir)
+    replacement_pairs = load_pairs(pairs_file)
+    apply_replacements(target_dir, replacement_pairs)
 
 
 class TranslationPostProcessingException(Exception):
@@ -106,11 +122,16 @@ class TranslationPostProcessingException(Exception):
         self.msg = msg
 
 
+def _run_unittests():
+    subprocess.call([sys.executable, 
+                     'scripts/test_translation_postprocessing.py'])
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="")
-    
-    parser.add_argument()
-    
-        
-    parser.parse_args()
-    main(topdir=topdir, termfile=termfile)
+    parser.add_argument("lang_dir",
+                help="The target language directory (e.g., `asa/ja-jp`)")
+    parser.add_argument("pairs_file",
+                help="JSON file containing search/replace pairs")
+    args = parser.parse_args()
+
+    main(lang_dir=lang_dir, pairs_file=pairs_file)
