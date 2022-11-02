3890aa3da461bcb386678fd7ed89a210f233fb0b
Ian Hakes <ian.hakes@okta.com>
2022-11-02 12:50:44 -0400

    Updates OCE for 2022.10.2 (Prod) - HF rebuild
    OKTA-544621

--- Resources/Scripts/require.config.js
+++ Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=638029182231968125'
+    urlArgs: 't=638029874596932362'
 });
--- en-us/Resources/Scripts/require.config.js
+++ en-us/Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=638029182203829091'
+    urlArgs: 't=638029874567511541'
 });
