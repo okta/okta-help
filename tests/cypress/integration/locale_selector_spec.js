919109d73024bd8e716e713af9210500d20bafeb
Paul Wallace <paul.wallace@okta.com>
2022-11-07 14:35:58 -0800

    Revising test for string change

--- tests/cypress/integration/locale_selector_spec.js
+++ tests/cypress/integration/locale_selector_spec.js
@@ -44,7 +44,7 @@ describe('Locale switching', () => {
     cy.visit('oie/en-us/Content/Topics/identity-engine/oie-index.htm')
     cy.switchLocale('Change language', '日本語 (日本)‎')
     cy.url().should('include', 'oie/ja-jp/Content/Topics/identity-engine/oie-index.htm')
-    cy.switchLocale('言語の変更', 'English (United States)')
+    cy.switchLocale('言語を変更', 'English (United States)')
     cy.url().should('include', 'oie/en-us/Content/Topics/identity-engine/oie-index.htm')
   })
 
@@ -52,7 +52,7 @@ describe('Locale switching', () => {
     cy.visit('oie/en-us/Content/Topics/ReleaseNotes/oie-relnotes.htm')
     cy.switchLocale('Change language', '日本語 (日本)‎')
     cy.url().should('include', 'oie/ja-jp/Content/Topics/ReleaseNotes/oie-relnotes.htm')
-    cy.switchLocale('言語の変更', 'English (United States)')
+    cy.switchLocale('言語を変更', 'English (United States)')
     cy.url().should('include', 'oie/en-us/Content/Topics/ReleaseNotes/oie-relnotes.htm')
   })
 
