4710a0eac92442e1ac8ca43b793f485bda45f91e
emikhasyak-okta <emikhasyak@okta.com>
2022-12-02 00:21:00 -0500

    2

--- ci-scripts/commit-artifact.sh
+++ ci-scripts/commit-artifact.sh
@@ -2,13 +2,17 @@
 export ARCHIVE_PATH="target.zip"
 export OUTPUT_FOLDER="../${PUBLISH_DESTINATION}"
 
+set -x
+cd ${OKTA_HOME}/${REPO}/asa/en-us/Content/Topics/Adv_Server_Access/docs/
+ls -l
+
+cd ${OKTA_HOME}/${REPO}/ci-scripts
 wget -O ${ARCHIVE_PATH} ${BUILT_ARTIFACT}
 
-set -x
 tar -xf ${ARCHIVE_PATH} -C ${OUTPUT_FOLDER} --strip-components=1 --overwrite  --no-same-permissions
 
 rm ${ARCHIVE_PATH}
-cd ${OKTA_HOME}/${REPO}/asa/en-us
+cd ${OKTA_HOME}/${REPO}/asa/en-us/Content/Topics/Adv_Server_Access/docs/
 ls -l
 
 cd ${OKTA_HOME}/${REPO}
