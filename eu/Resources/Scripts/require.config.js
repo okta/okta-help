9121cb8e7d03187f55c4aa6af1713664a4f83232
CI Automation <eng_prod@okta.com>
2022-12-02 19:25:50 +0000

    Updates eu

--- eu/Resources/Scripts/require.config.js
+++ eu/Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=638043871090338180'
+    urlArgs: 't=638056056224857781'
 });
--- eu/en-us/Resources/Scripts/require.config.js
+++ eu/en-us/Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=638043871052198108'
+    urlArgs: 't=638056056181547768'
 });
