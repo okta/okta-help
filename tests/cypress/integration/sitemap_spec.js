b36ce782c57f1e893a75b69f3f09e882555f39e3
emikhasyak-okta <emikhasyak@okta.com>
2022-12-08 21:07:40 -0500

    Verifies 20 urls from every en sitemap (#650)
    OKTA-555977 Verifies 20 urls from every en sitemap

---
+++ tests/cypress/integration/sitemap_spec.js
@@ -0,0 +1,35 @@
+describe('Sitemaps validation', () => {
+  const NUMBER_OF_ENTITIES_TO_CHECK = 20
+  const verifySitemap = (path) => {
+    cy.readFile(path).then((response) => {
+      // convert sitemap xml body to an array of urls
+      const urls = Cypress.$(response)
+        // according to the sitemap.xml spec,
+        // the url value should reside in a <loc /> node
+        // https://www.google.com/sitemaps/protocol.html
+        .find('loc')
+        .toArray()
+        // get the text of the <loc /> node
+        .map((el) => el.innerText)
+
+      const step = Math.floor(urls.length / NUMBER_OF_ENTITIES_TO_CHECK) + 1
+
+      for (let i = 0; i < urls.length; i += step) {
+        cy.request('GET', urls[i])
+      }
+    })
+  }
+
+  [
+    'en-us/Sitemap.xml',
+    'asa/en-us/Sitemap.xml',
+    'eu/en-us/Sitemap.xml',
+    'oag/en-us/Sitemap.xml',
+    'oie/en-us/Sitemap.xml',
+    'wf/en-us/Sitemap.xml',
+  ].forEach((sitemap) => {
+    it(`Loads ${NUMBER_OF_ENTITIES_TO_CHECK} urls from ${sitemap}`, () => {
+      verifySitemap(sitemap)
+    })
+  })
+})
