88567301ca6893b4b55cca58bf1646826063dd97
emikhasyak-okta <emikhasyak@okta.com>
2023-01-04 13:46:22 -0500

    1

--- ci-scripts/request-translation.sh
+++ ci-scripts/request-translation.sh
@@ -30,6 +30,7 @@ export RESOURCE_PATHS=( "Content/Resources" "Resources" "Data" "Skins" "Sitemap.
 for RESOURCE_PATH in "${RESOURCE_PATHS[@]}"
 do
    :
+   echo skip
   # rsync -av --exclude "Tocs/" "${EN_PATH}/${RESOURCE_PATH}" "${JA_PATH}"
 done
 
