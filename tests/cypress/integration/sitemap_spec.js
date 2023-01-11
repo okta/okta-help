d369f91ceb6b6810981f4c9f287445aeef830cdb
Paul Wallace <paul.wallace@okta.com>
2023-01-10 19:10:06 -0800

    Refactoring sitemap tests (#725)

--- tests/cypress/integration/sitemap_spec.js
+++ tests/cypress/integration/sitemap_spec.js
@@ -14,19 +14,32 @@ describe('Sitemaps validation', () => {
 
       const step = Math.floor(urls.length / NUMBER_OF_ENTITIES_TO_CHECK) + 1
 
+      // We call this once here to establish the base URL
+      cy.visit('http://localhost:8000')
+
       for (let i = 0; i < urls.length; i += step) {
-        cy.request('GET', urls[i])
+        const parts = urls[i].split('https://help.okta.com')
+        // Use the relative path split from the base H.O.C. URL
+        const relpath = parts[1]
+
+        cy.request('GET', relpath)
       }
     })
   }
 
   [
     'en-us/Sitemap.xml',
+    // 'ja-jp/Sitemap.xml', enable when we post baseline translation
     'asa/en-us/Sitemap.xml',
+    'asa/ja-jp/Sitemap.xml',
     'eu/en-us/Sitemap.xml',
+    'eu/ja-jp/Sitemap.xml',
     'oag/en-us/Sitemap.xml',
+    // 'oag/ja-jp/Sitemap.xml', enable when we post baseline translation
     'oie/en-us/Sitemap.xml',
+    'oie/ja-jp/Sitemap.xml',
     'wf/en-us/Sitemap.xml',
+    // 'wf/ja-jp/Sitemap.xml' enable when we post baseline translation
   ].forEach((sitemap) => {
     it(`Loads ${NUMBER_OF_ENTITIES_TO_CHECK} urls from ${sitemap}`, () => {
       verifySitemap(sitemap)
