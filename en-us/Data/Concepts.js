154069564b62cb559d39325155ad7d22acf381a4
CI Automation <eng_prod@okta.com>
2023-01-27 22:46:10 +0000

    Updates prod-oce

--- en-us/Data/Concepts.js
+++ en-us/Data/Concepts.js
@@ -1,1 +1,1 @@
-define({'ad-agent':[70,72,1089],'dir-intg':[115,49],});
+define({'ad-agent':[1089,72,70],'dir-intg':[115,49],});
--- en-us/Resources/Scripts/require.config.js
+++ en-us/Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=638102382728448636'
+    urlArgs: 't=638104559384502439'
 });
