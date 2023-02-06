11f929eb7ac63b91b32b5bc49bb497616058300d
emikhasyak-okta <emikhasyak@okta.com>
2023-02-06 11:23:26 -0500

    1

--- ci-scripts/commit-artifact.sh
+++ ci-scripts/commit-artifact.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-export ARCHIVE_PATH="target.zip"
-export OUTPUT_FOLDER="../${PUBLISH_DESTINATION}"
+#export ARCHIVE_PATH="target.zip"
+export PUBLISH_TARGETS_FILE="publish_targets.txt"
+#export OUTPUT_FOLDER="../${PUBLISH_DESTINATION}"
+
+copy_artifact()
+{
+  exoprt OUT_FOLDER = $1
+  export ARTIFACT_URL = $2
+  export ARCHIVE_PATH="target.zip"
+
+  mkdir -p "${OUT_FOLDER}"
+  wget -O ${ARCHIVE_PATH} ${ARTIFACT_URL}
+  tar -xf ${ARCHIVE_PATH} -C ${OUT_FOLDER} --strip-components=1 --overwrite  --no-same-permissions
+  rm ${ARCHIVE_PATH}
+}
 
 cd ${OKTA_HOME}/${REPO}
 git fetch origin ${TOPIC_BRANCH}
@@ -17,12 +30,28 @@ fi
 
 cd ${OKTA_HOME}/${REPO}/ci-scripts
 
-mkdir -p "${OUTPUT_FOLDER}"
+if [ ${PUBLISH_TARGETS} ]
+then
+  wget -O ${PUBLISH_TARGETS_FILE} ${PUBLISH_TARGETS}
+  while IFS=$' \t\r\n' read -r artifact_link publish_to
+  do
+    copy_artifact "../${publish_to}" "${artifact_link}"
 
-wget -O ${ARCHIVE_PATH} ${BUILT_ARTIFACT}
-tar -xf ${ARCHIVE_PATH} -C ${OUTPUT_FOLDER} --strip-components=1 --overwrite  --no-same-permissions
+    # OUTPUT_FOLDER="../${publish_to}"
 
-rm ${ARCHIVE_PATH}
+    # mkdir -p "${OUTPUT_FOLDER}"
+    # wget -O ${ARCHIVE_PATH} ${artifact_link}
+    # tar -xf ${ARCHIVE_PATH} -C ${OUTPUT_FOLDER} --strip-components=1 --overwrite  --no-same-permissions
+    # rm ${ARCHIVE_PATH}
+  done < ${PUBLISH_TARGETS_FILE}
+else
+  copy_artifact "../${PUBLISH_DESTINATION}" "${BUILT_ARTIFACT}"
+  # mkdir -p "${OUTPUT_FOLDER}"
+  # wget -O ${ARCHIVE_PATH} ${BUILT_ARTIFACT}
+  # tar -xf ${ARCHIVE_PATH} -C ${OUTPUT_FOLDER} --strip-components=1 --overwrite  --no-same-permissions
+  # rm ${ARCHIVE_PATH}
+fi
+
 cd ${OKTA_HOME}/${REPO}
 
 git add --all
