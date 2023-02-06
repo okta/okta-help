5c759b91ca48298da0cb1e4ad95d0219f367f7f5
emikhasyak-okta <emikhasyak@okta.com>
2023-02-06 12:05:54 -0500

    2

--- ci-scripts/commit-artifact.sh
+++ ci-scripts/commit-artifact.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-#export ARCHIVE_PATH="target.zip"
+
 export PUBLISH_TARGETS_FILE="publish_targets.txt"
-#export OUTPUT_FOLDER="../${PUBLISH_DESTINATION}"
 
 copy_artifact()
 {
@@ -36,20 +35,9 @@ then
   while IFS=$' \t\r\n' read -r artifact_link publish_to
   do
     copy_artifact "../${publish_to}" "${artifact_link}"
-
-    # OUTPUT_FOLDER="../${publish_to}"
-
-    # mkdir -p "${OUTPUT_FOLDER}"
-    # wget -O ${ARCHIVE_PATH} ${artifact_link}
-    # tar -xf ${ARCHIVE_PATH} -C ${OUTPUT_FOLDER} --strip-components=1 --overwrite  --no-same-permissions
-    # rm ${ARCHIVE_PATH}
   done < ${PUBLISH_TARGETS_FILE}
 else
   copy_artifact "../${PUBLISH_DESTINATION}" "${BUILT_ARTIFACT}"
-  # mkdir -p "${OUTPUT_FOLDER}"
-  # wget -O ${ARCHIVE_PATH} ${BUILT_ARTIFACT}
-  # tar -xf ${ARCHIVE_PATH} -C ${OUTPUT_FOLDER} --strip-components=1 --overwrite  --no-same-permissions
-  # rm ${ARCHIVE_PATH}
 fi
 
 cd ${OKTA_HOME}/${REPO}
