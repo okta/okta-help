d007152e9009dbd1c44e394552b2f84d6269fa55
CI Automation <eng_prod@okta.com>
2023-02-10 17:53:54 +0000

    Updates prod-wf

--- wf/Resources/Scripts/require.config.js
+++ wf/Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=638114936648548117'
+    urlArgs: 't=638116477812949891'
 });
--- wf/en-us/Resources/Scripts/require.config.js
+++ wf/en-us/Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=638114936623794260'
+    urlArgs: 't=638116477777970180'
 });
