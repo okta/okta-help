4808d8bbb934e13aba8dfe71e7dedaedffceab1b
emikhasyak-okta <emikhasyak@okta.com>
2023-03-11 21:17:18 -0500

    123

--- ci-scripts/receive-translation-ll.sh
+++ ci-scripts/receive-translation-ll.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 set -x
-pushd ${OKTA_HOME}/${REPO}
 
-export SLACK_CHANNEL='#infodev-notifications'
-export targets=( "oce" "asa" "eu" "oie" "wf" "oag" )
-if [[ ! "${targets[*]}" =~ "${TARGET}" ]]; then
-  echo "No such target ${TARGET}. Exiting."
-  exit ${FAILED_SETUP}
-fi
+export BACON_TASK_NAME="CI_DOC_TOOLS_RECEIVE_TRANSLATION_LL"
 
-export TARGET_PATH=${TARGET}"/"
-if [ ${TARGET} == "oce" ]; then
-  TARGET_PATH=''
-fi
+source setup-translation-ll.sh
 
-export EN_PATH="${TARGET_PATH}en-us"
-export JA_PATH="${TARGET_PATH}ja-jp"
-export TRANSLATION_BRANCH=em-translations-${TARGET}
-export TRANSLATION_COMMITS="https://github.com/okta/okta-help/commits/${TRANSLATION_BRANCH}"
+
+# pushd ${OKTA_HOME}/${REPO}
+
+# export SLACK_CHANNEL='#infodev-notifications'
+# export targets=( "oce" "asa" "eu" "oie" "wf" "oag" )
+# if [[ ! "${targets[*]}" =~ "${TARGET}" ]]; then
+#   echo "No such target ${TARGET}. Exiting."
+#   exit ${FAILED_SETUP}
+# fi
+
+# export TARGET_PATH=${TARGET}"/"
+# if [ ${TARGET} == "oce" ]; then
+#   TARGET_PATH=''
+# fi
+
+# export EN_PATH="${TARGET_PATH}en-us"
+# export JA_PATH="${TARGET_PATH}ja-jp"
+# export TRANSLATION_BRANCH=em-translations-${TARGET}
+# export TRANSLATION_COMMITS="https://github.com/okta/okta-help/commits/${TRANSLATION_BRANCH}"
 
 export TRANSLATION_RECEIVING_BRANCH="em-translations-${TARGET}-receive-$(TZ=UTC+8 date +'%Y-%m-%d_%H-%M-%S_%s')"
 
@@ -63,7 +69,7 @@ then
   exit ${FAILED_SETUP}
 fi
 
-export BACON_LINK="https://bacon-go.aue1e.saasure.net/tasks/CI_DOC_TOOLS_RECEIVE_TRANSLATION_LL?taskId=${TEST_SUITE_RESULT_ID}"
+#export BACON_LINK="https://bacon-go.aue1e.saasure.net/tasks/CI_DOC_TOOLS_RECEIVE_TRANSLATION_LL?taskId=${TEST_SUITE_RESULT_ID}"
 
 send_slack_message "${SLACK_CHANNEL}" \
   ":white_check_mark: [${TARGET^^}] translation is ready for review" \
