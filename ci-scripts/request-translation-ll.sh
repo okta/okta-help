8083fb5c3f243d9975e23a752f1486d37bcb55d7
emikhasyak-okta <emikhasyak@okta.com>
2023-03-11 21:06:21 -0500

    123

--- ci-scripts/request-translation-ll.sh
+++ ci-scripts/request-translation-ll.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 set -x
-pushd ${OKTA_HOME}/${REPO}
 
-export SLACK_CHANNEL='#infodev-notifications'
-export targets=( "oce" "asa" "eu" "oie" "wf" "oag" )
-if [[ ! "${targets[*]}" =~ "${TARGET}" ]]; then
-  echo "No such target ${TARGET}. Exiting."
-  exit ${FAILED_SETUP}
-fi
+export BACON_TASK_NAME="CI_DOC_TOOLS_REQUEST_TRANSLATION_LL"
 
-export TARGET_PATH=${TARGET}"/"
-if [ ${TARGET} == "oce" ]; then
-  TARGET_PATH=''
-fi
+source stupe-translation-ll.sh
 
-export EN_PATH="${TARGET_PATH}en-us"
-export JA_PATH="${TARGET_PATH}ja-jp"
-export TRANSLATION_BRANCH=em-translations-${TARGET}
-export TRANSLATION_COMMITS="https://github.com/okta/okta-help/commits/${TRANSLATION_BRANCH}"
+
+# pushd ${OKTA_HOME}/${REPO}
+
+# export SLACK_CHANNEL='#infodev-notifications'
+# export targets=( "oce" "asa" "eu" "oie" "wf" "oag" )
+# if [[ ! "${targets[*]}" =~ "${TARGET}" ]]; then
+#   echo "No such target ${TARGET}. Exiting."
+#   exit ${FAILED_SETUP}
+# fi
+
+# export TARGET_PATH=${TARGET}"/"
+# if [ ${TARGET} == "oce" ]; then
+#   TARGET_PATH=''
+# fi
+
+# export EN_PATH="${TARGET_PATH}en-us"
+# export JA_PATH="${TARGET_PATH}ja-jp"
+# export TRANSLATION_BRANCH=em-translations-${TARGET}
+
+#export BACON_LINK="https://bacon-go.aue1e.saasure.net/tasks/${BACON_TASK_NAME}?taskId=${TEST_SUITE_RESULT_ID}"
+
+#export TRANSLATION_COMMITS="https://github.com/okta/okta-help/commits/${TRANSLATION_BRANCH}"
 
 git fetch --depth=1 origin ${TRANSLATION_BRANCH}
 git reset --hard FETCH_HEAD
@@ -41,24 +50,22 @@ pushd ${EN_PATH}
 git restore --source origin/${SHA} -- . ':!*/Topics/ReleaseNotes/*'
 popd
 
-export BACON_LINK="https://bacon-go.aue1e.saasure.net/tasks/CI_DOC_TOOLS_REQUEST_TRANSLATION_LL?taskId=${TEST_SUITE_RESULT_ID}"
-
-# somehow makes git diff-index work properly
-git status
+# somehow this call makes git diff-index work properly all the time
+git status -s
 
 if git diff-index --quiet HEAD --; then
   echo 'No changes detected in [${EN_PATH}]'
 
-  exit
   send_slack_message "${SLACK_CHANNEL}" \
     ":warning: No changes for [${TARGET^^}]" \
     "Commit author: ${userEmail}\nTranslation commits: ${TRANSLATION_COMMITS}\nBacon task: ${BACON_LINK}" \
     "warning"
   exit
 fi
-exit
+
 git add --all
-git -c user.name='CI Automation' -c user.email=${userEmail} commit -m "$(TZ=UTC+8 date +'%Y-%m-%d %H:%M:%S') Copying en resources and files for ${TARGET^^} project"
+git -c user.name='CI Automation' -c user.email=${userEmail} \
+  commit -m "$(TZ=UTC+8 date +'%Y-%m-%d %H:%M:%S') Copying en resources and files for ${TARGET^^} project"
 git push origin ${TRANSLATION_BRANCH}
 
 send_slack_message "${SLACK_CHANNEL}" \
