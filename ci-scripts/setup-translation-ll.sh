4862d1553bdb5841c729c7cb4ad7209e0125fbb0
emikhasyak-okta <emikhasyak@okta.com>
2023-03-16 10:51:50 -0400

    Updates scripts with the latest from gh-pages

---
+++ ci-scripts/setup-translation-ll.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+pushd ${OKTA_HOME}/${REPO}
+
+export SLACK_CHANNEL='#infodev-notifications'
+export targets=( "oce" "asa" "eu" "oie" "wf" "oag" )
+if [[ ! "${targets[*]}" =~ "${TARGET}" ]]; then
+  echo "No such target ${TARGET}. Exiting."
+  exit ${FAILED_SETUP}
+fi
+
+export TARGET_PATH=${TARGET}"/"
+if [ ${TARGET} == "oce" ]; then
+  TARGET_PATH=''
+fi
+
+export TRANSLATION_BRANCH=em-translations-${TARGET}
+export EN_PATH="${TARGET_PATH}en-us"
+export JA_PATH="${TARGET_PATH}ja-jp"
+
+export TRANSLATION_COMMITS="https://github.com/okta/okta-help/commits/${TRANSLATION_BRANCH}"
+export BACON_LINK="https://bacon-go.aue1e.saasure.net/tasks/${BACON_TASK_NAME}?taskId=${TEST_SUITE_RESULT_ID}"
