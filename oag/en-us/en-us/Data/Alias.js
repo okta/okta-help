2c2ecb57a9fa31b0d9e65769c5f9bb696723c9c7
CI Automation <eng_prod@okta.com>
2023-03-31 03:58:43 +0000

    Updates prod-oag

---
+++ oag/en-us/en-us/Data/Alias.js
@@ -0,0 +1,8 @@
+var xmlAliasData = "";
+xmlAliasData += '<?xml version=\"1.0\" encoding=\"utf-8\"?>';
+xmlAliasData += '<CatapultAliasFile>';
+xmlAliasData += '    <Map Name=\"ext_oag_integrate_apps\" Link=\"Topics/Access-Gateway/integrate-apps-with-oag.htm\" ResolvedId=\"3055\" />';
+xmlAliasData += '    <Map Name=\"ext_oag_main\" Link=\"Topics/Access-Gateway/ag-main.htm\" ResolvedId=\"3074\" />';
+xmlAliasData += '    <Map Name=\"ext_oag_manage_deployment\" Link=\"Topics/Access-Gateway/manage-deployment.htm\" ResolvedId=\"3068\" />';
+xmlAliasData += '</CatapultAliasFile>';
+MadCap.Utilities.Xhr._FilePathToXmlStringMap.Add('Alias', xmlAliasData);
