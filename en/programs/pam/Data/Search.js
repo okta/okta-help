a3aa1acf7332fa9e44369344c9809c53a1528d82
eng-prod-CI-bot-okta <50187356+eng-prod-CI-bot-okta@users.noreply.github.com>
2023-04-14 12:59:13 -0700

    Updates PROD-BETA-CLOSED-PAM - build date: 2023.04.14 (#1050)
    Updates prod-beta-closed-pam
    
    Co-authored-by: CI Automation <eng_prod@okta.com>

--- en/programs/pam/Data/Search.js
+++ en/programs/pam/Data/Search.js
@@ -1,1 +1,1 @@
-define({r:1,st:'Stem',n:1,t:[0],m:[],u:['../Content/Topics/betas/closed/privileged-access/pam-access-request.htm'],s:['$home'],p:[['$home',27]]});
+define({r:1,st:'Stem',n:1,t:[0],m:[],u:['../Content/Topics/betas/closed/privileged-access/pam-access-request.htm'],s:['$basearch'],p:[['$basearch',19]]});
