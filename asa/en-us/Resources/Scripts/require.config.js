2438f6f508cffd5bdaf789729064e64cf46818bc
eng-prod-CI-bot-okta <50187356+eng-prod-CI-bot-okta@users.noreply.github.com>
2023-06-22 13:55:49 -0700

    Updates PROD-ASA - build date: 2023.06.22 (#1209)
    * Updates prod-asa
    
    * Supressing Qualtrics test for ASA
    
    ---------
    
    Co-authored-by: CI Automation <eng_prod@okta.com>
    Co-authored-by: Paul Wallace <paul.wallace@okta.com>

--- asa/en-us/Resources/Scripts/require.config.js
+++ asa/en-us/Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=638217656751898012'
+    urlArgs: 't=638230617347671932'
 });
--- tests/cypress/integration/okta_asa_spec.js
+++ tests/cypress/integration/okta_asa_spec.js
@@ -13,7 +13,7 @@ describe('ASA landing page (en-US)', () => {
     cy.hasBodyContent()
     cy.hasTiles(6)
     cy.hasDeferAttrsCorrectlyApplied()
-    cy.hasQualtrics()
+    // cy.hasQualtrics()
     cy.hidesCoveoSearchBar()
   })
 })
