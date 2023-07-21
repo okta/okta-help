52f35e424a6728426e7b125e23c47615c56d4ef8
Paul Wallace <paul.wallace@okta.com>
2023-07-21 15:52:43 -0700

    Updating Qualtrics tests

--- tests/cypress/support/commands.js
+++ tests/cypress/support/commands.js
@@ -162,8 +162,16 @@ function hasDeferAttrsCorrectlyApplied () {
 }
 
 function hasQualtrics () {
-  cy.get('body script[src*="qualtrics.js"]')
-    .should('exist')
+  cy.get('div#feedback-tab')
+    .should('be.visible')
+
+  cy.get('a#feedback-link')
+    .invoke('attr', 'href')
+    .and('eq', '#')
+
+  cy.get('a#feedback-link')
+    .should('be.visible')
+    .should('have.attr', 'onclick')
 }
 
 function hasCoveoSearchBar () {
