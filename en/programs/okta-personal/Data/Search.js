db819546fb7470184a6ba3f91359f0da16c16048
Paul Wallace <paul.wallace@okta.com>
2023-08-01 15:43:12 -0700

    Remove Okta Personal artifacts (#1347)

--- en/programs/okta-personal/Data/Search.js
+++
@@ -1,1 +0,0 @@
-define({r:1,st:'Stem',n:1,t:[0],m:[],u:['../Content/Topics/okta-personal/add-apps.htm'],s:['0'],p:[['0',8]]});
--- en/programs/okta-personal/Resources/Scripts/require.config.js
+++
@@ -1,3 +0,0 @@
-require.config({
-    urlArgs: 't=638249667066748532'
-});
