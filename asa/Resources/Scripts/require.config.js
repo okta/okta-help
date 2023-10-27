653e7ffe2f8778ed7060352cbfcf53a71ea2a824
CI Automation <eng_prod@okta.com>
2023-10-27 15:09:00 +0000

    Updates prod-asa

--- asa/Resources/Scripts/require.config.js
+++ asa/Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=638340112953749601'
+    urlArgs: 't=638340159107974871'
 });
--- asa/en-us/Resources/Scripts/require.config.js
+++ asa/en-us/Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=638340112919189545'
+    urlArgs: 't=638340159071835118'
 });
