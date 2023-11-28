c135894c8eb6a6b6dd74b8271ec7be0d97b92d26
CI Automation <eng_prod@okta.com>
2023-11-28 22:23:05 +0000

    Updates prod-oag

--- oag/Resources/Scripts/require.config.js
+++ oag/Resources/Scripts/require.config.js
@@ -1,7 +1,3 @@
 require.config({
-<<<<<<< HEAD
-    urlArgs: 't=638367949913674971'
-=======
-    urlArgs: 't=638362055326490230'
->>>>>>> ad478fd7a75807f01564ff0a1c2d1ac6a49ba2ef
+    urlArgs: 't=638368064238976985'
 });
