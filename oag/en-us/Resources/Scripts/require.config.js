d5365cc0b1c4b1d53c38b18546166795c9df0d05
eng-prod-CI-bot-okta <50187356+eng-prod-CI-bot-okta@users.noreply.github.com>
2023-11-28 14:48:44 -0800

    Updates PROD-OAG - build date: 2023.11.28 (#1669)
    * Updates prod-oag
    
    * Updates prod-oag
    
    * Updates prod-oag
    
    ---------
    
    Co-authored-by: CI Automation <eng_prod@okta.com>
    Co-authored-by: Ian Hakes <ian.hakes@okta.com>

--- oag/en-us/Resources/Scripts/require.config.js
+++ oag/en-us/Resources/Scripts/require.config.js
@@ -1,7 +1,3 @@
 require.config({
-<<<<<<< HEAD
-    urlArgs: 't=638367949872794983'
-=======
-    urlArgs: 't=638362055283300232'
->>>>>>> ad478fd7a75807f01564ff0a1c2d1ac6a49ba2ef
+    urlArgs: 't=638368064192287053'
 });
