1ea6f403ef13a626fee3dafbaa118ddd98dbbb00
CI Automation <paul.wallace@okta.com>
2023-11-30 22:25:08 +0000

    2023-11-30 14:25:08 Receiving translation for OIE project

--- oie/ja-jp/Data/Search.js
+++ oie/ja-jp/Data/Search.js
@@ -1,1 +1,1 @@
-define({r:1,st:'Stem',n:1,t:[0],m:[],u:['../content/topics/api/trusted-origins-iframe.htm'],s:['$'],p:[['$',7]]});
+define({r:1,st:'Stem',n:1,t:[0],m:[],u:['../content/topics/api/trusted-origins-iframe.htm'],s:['$'],p:[['$',3]]});
