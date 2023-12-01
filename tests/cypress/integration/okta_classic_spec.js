b99a73a024a00cf29487c587d09e08c2d5cf693b
Paul Wallace <paul.wallace@okta.com>
2023-12-01 11:49:17 -0800

    updating cypress tests

--- tests/cypress/integration/okta_classic_spec.js
+++ tests/cypress/integration/okta_classic_spec.js
@@ -29,7 +29,7 @@ describe('Classic landing page (ja-JP)', () => {
     cy.hasLeftSideNav()
     cy.hasTOC(9)
     cy.hasBreadcrumbs('Okta Classic Engine')
-    cy.hasTopMenuBar(5, 11)
+    cy.hasTopMenuBar(6, 10)
     cy.hasBodyContent()
     cy.hasTiles(9)
     cy.hasDeferAttrsCorrectlyApplied()
