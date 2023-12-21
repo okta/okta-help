d5b3e9be9ec77b0442fa922afbb8902e6e8ffb52
eng-prod-CI-bot-okta <50187356+eng-prod-CI-bot-okta@users.noreply.github.com>
2023-12-21 09:59:14 -0800

    Updates PROD-WF - build date: 2023.12.21 (#1761)
    Updates prod-wf
    
    Co-authored-by: CI Automation <eng_prod@okta.com>

--- wf/Resources/Scripts/require.config.js
+++ wf/Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=638387104630813281'
+    urlArgs: 't=638387752793668646'
 });
--- wf/en-us/Resources/Scripts/require.config.js
+++ wf/en-us/Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=638387104592753883'
+    urlArgs: 't=638387752754268693'
 });
