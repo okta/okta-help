500306e014e0e834b337b494c50580da2830a284
CI Automation <eng_prod@okta.com>
2024-04-05 15:46:39 +0000

    Updates prod-oag

--- oag/en-us/Data/Search.js
+++ oag/en-us/Data/Search.js
@@ -1,1 +1,1 @@
-define({r:1,st:'Stem',n:1,t:[0],m:[],u:['../content/topics/access-gateway/about-access-gateway-applications.htm'],s:['$'],p:[['$',1]]});
+define({r:1,st:'Stem',n:1,t:[0],m:[],u:['../content/topics/access-gateway/ag-main.htm'],s:['access'],p:[['access',0]]});
--- oag/en-us/Resources/Scripts/require.config.js
+++ oag/en-us/Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=638467184265280091'
+    urlArgs: 't=638479285562359636'
 });
