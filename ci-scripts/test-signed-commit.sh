4a73b9ef0786c5063950d72c80dd589a927edf93
emikhasyak-okta <emikhasyak@okta.com>
2024-04-19 13:23:12 -0400

    123

--- ci-scripts/test-signed-commit.sh
+++ ci-scripts/test-signed-commit.sh
@@ -4,8 +4,8 @@ git checkout -b ${TOPIC_BRANCH}
 
 touch $(TZ=UTC+8 date +'%Y-%m-%d_%H-%M-%S')
 
-git add --all
-git -c user.name='CI Automation' -c user.email=${userEmail} commit -m "test"
-git push origin ${TOPIC_BRANCH}
+# git add --all
+# git -c user.name='CI Automation' -c user.email=${userEmail} commit -m "test"
+# git push origin ${TOPIC_BRANCH}
 
 commit_sign_push "signed commit"
