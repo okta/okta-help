226a5735874a884388fab9b8cfe7940e67df2524
emikhasyak-okta <emikhasyak@okta.com>
2024-04-22 19:03:23 -0400

    test

--- ci-scripts/receive-translation-ll.sh
+++ ci-scripts/receive-translation-ll.sh
@@ -6,28 +6,26 @@ source setup-translation-ll.sh
 export TRANSLATION_RECEIVING_BRANCH="translations-${TARGET}-receive-$(TZ=UTC+8 date +'%Y-%m-%d_%H-%M-%S_%s')"
 
 # topic from latest main
-#git fetch --depth=1 origin ${BASE_BRANCH}
-git fetch origin ${BASE_BRANCH}
+git fetch --depth=1 origin ${BASE_BRANCH}
 git switch ${BASE_BRANCH}
 git checkout -b ${TRANSLATION_RECEIVING_BRANCH}
 
-#git fetch --depth=1 origin ${TRANSLATION_BRANCH}
-#git fetch origin ${TRANSLATION_BRANCH}
+git fetch --depth=1 origin ${TRANSLATION_BRANCH}
 
 # get en-us sitemap for syncing ja-jp content
-#git restore --source origin/${TRANSLATION_BRANCH} -- ${EN_PATH}/Sitemap.xml
+git restore --source origin/${TRANSLATION_BRANCH} -- ${EN_PATH}/Sitemap.xml
 
-# get ja-jp files from translation branch
-# pushd ${JA_PATH}
-# git restore --source origin/${TRANSLATION_BRANCH} -- .
-# popd
+get ja-jp files from translation branch
+pushd ${JA_PATH}
+git restore --source origin/${TRANSLATION_BRANCH} -- .
+popd
 
-# run post processing
-# yum -y install python3-devel
-# python3 scripts/translation_postprocessing.py ${TARGET}
+run post processing
+yum -y install python3-devel
+python3 scripts/translation_postprocessing.py ${TARGET}
 
 # revert en-us sitemap
-#git checkout -- ${EN_PATH}/Sitemap.xml
+git checkout -- ${EN_PATH}/Sitemap.xml
 
 touch $(TZ=UTC+8 date +'%Y-%m-%d_%H-%M-%S')
 
