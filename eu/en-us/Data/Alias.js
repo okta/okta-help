afe0cd43a32577647895818ea3b8ac824b0df617
CI Automation <eng-info-dev-github-bot@okta.com>
2024-04-30 22:24:55 +0000

    Updates prod-enduser

--- eu/en-us/Data/Alias.js
+++ eu/en-us/Data/Alias.js
@@ -34,7 +34,6 @@ xmlAliasData += '    <Map Name=\"csh-user-dashboard-password-reset\" Link=\"topi
 xmlAliasData += '    <Map Name=\"csh-user-dashboard-set-language\" Link=\"topics/end-user/manage-account-settings.htm#Select\" ResolvedId=\"4014\" />';
 xmlAliasData += '    <Map Name=\"csh-user-generate-random-password\" Link=\"topics/end-user/generate-random-password.htm\" ResolvedId=\"4006\" />';
 xmlAliasData += '    <Map Name=\"csh-user-home\" Link=\"topics/end-user/end-user-home.htm\" ResolvedId=\"4016\" />';
-xmlAliasData += '    <Map Name=\"csh-user-om-omm-user\" Link=\"topics/end-user/om-omm-user.htm\" ResolvedId=\"4004\" />';
 xmlAliasData += '    <Map Name=\"csh-user-om-overview\" Link=\"topics/end-user/om-overview.htm\" ResolvedId=\"4002\" />';
 xmlAliasData += '    <Map Name=\"csh-user-om-user-safari-ext\" Link=\"topics/end-user/om-user-safari-ext.htm\" ResolvedId=\"4003\" />';
 xmlAliasData += '    <Map Name=\"csh-user-ov-manage-account\" Link=\"topics/end-user/ov-overview.htm\" ResolvedId=\"12004\" />';
@@ -82,7 +81,6 @@ xmlAliasData += '    <Map Name=\"ext-plugin-config-settings\" Link=\"topics/end-
 xmlAliasData += '    <Map Name=\"ext-plugin-download-install\" Link=\"topics/end-user/plugin-download_install.htm\" ResolvedId=\"4042\" />';
 xmlAliasData += '    <Map Name=\"ext-random-password\" Link=\"topics/end-user/generate-random-password.htm\" ResolvedId=\"4060\" />';
 xmlAliasData += '    <Map Name=\"ext-user-plugin-keyboard\" Link=\"topics/end-user/plugin-keyboard.htm\" ResolvedId=\"4087\" />';
-xmlAliasData += '    <Map Name=\"ext_Apps_OMM_End_User\" Link=\"topics/end-user/om-omm-user.htm\" ResolvedId=\"200108\" />';
 xmlAliasData += '    <Map Name=\"ext_end_user_okta_account_chooser\" Link=\"topics/end-user/plugin-switch-accounts.htm\" ResolvedId=\"200180\" />';
 xmlAliasData += '    <Map Name=\"ext_End_User_Password_Reset\" Link=\"topics/end-user/manage-account-settings.htm#Reset\" ResolvedId=\"200181\" />';
 xmlAliasData += '    <Map Name=\"ext_ov_endusers\" Link=\"topics/end-user/ov-overview.htm\" ResolvedId=\"1212\" />';
