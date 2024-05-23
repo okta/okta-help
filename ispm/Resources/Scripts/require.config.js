f6bd192f40692e6781194e7cb69cc70f8ff00e7e
CI Automation <eng-info-dev-github-bot@okta.com>
2024-05-23 21:59:08 +0000

    Updates prod-ispm

--- ispm/Resources/Scripts/require.config.js
+++ ispm/Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=638519952393758715'
+    urlArgs: 't=638520982907297133'
 });
--- ispm/en-us/Resources/Scripts/require.config.js
+++ ispm/en-us/Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=638519952347472440'
+    urlArgs: 't=638520982862033652'
 });
