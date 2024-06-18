a2d4f885ec982d658a04ea004c9e50f2b0960e1d
CI Automation <eng-info-dev-github-bot@okta.com>
2024-06-18 22:19:36 +0000

    2024-06-18 14:19:36 Receiving translation for OCE project

--- ja-jp/Data/Search.js
+++ ja-jp/Data/Search.js
@@ -1,1 +1,1 @@
-define({r:1,st:'Stem',n:1,t:[0],m:[],u:['../content/index-admin.htm'],s:['$'],p:[['$',12]]});
+define({r:1,st:'Stem',n:1,t:[0],m:[],u:['../content/index-admin.htm'],s:['$'],p:[['$',13]]});
