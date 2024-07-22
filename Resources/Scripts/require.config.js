f324f7b741d9a63108cb9c42f6abdb33410a631b
CI Automation <eng-info-dev-github-bot@okta.com>
2024-07-22 18:20:08 +0000

    Updates prod-bigtwo

--- Resources/Scripts/require.config.js
+++ Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=638570043004985792'
+    urlArgs: 't=638572662366409580'
 });
--- oie/Resources/Scripts/require.config.js
+++ oie/Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=638570069238423294'
+    urlArgs: 't=638572689330404707'
 });
