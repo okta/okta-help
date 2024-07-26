b9aa9e40ff1d17f0d46583d8b473d9d67860f29b
CI Automation <eng-info-dev-github-bot@okta.com>
2024-07-26 22:48:03 +0000

    Updates prod-bigtwo

--- oie/en-us/Data/Search.js
+++ oie/en-us/Data/Search.js
@@ -1,1 +1,1 @@
-define({r:1,st:'Stem',n:1,t:[0],m:[],u:['../content/topics/api/trusted-origins-iframe.htm'],s:['$'],p:[['$',12]]});
+define({r:1,st:'Stem',n:1,t:[0],m:[],u:['../content/topics/identity-engine/authenticators/authentication-main.htm'],s:['10'],p:[['10',6]]});
