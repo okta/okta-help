a859244cfa25528bec7bc4fa0309a269e66cbf17
CI Automation <eng-info-dev-github-bot@okta.com>
2024-08-21 20:33:44 +0000

    Updates prod-oag

--- oag/Resources/Scripts/require.config.js
+++ oag/Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=638592605888841834'
+    urlArgs: 't=638598691660894827'
 });
--- oag/en-us/Resources/Scripts/require.config.js
+++ oag/en-us/Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=638592605784410733'
+    urlArgs: 't=638598691551917319'
 });
