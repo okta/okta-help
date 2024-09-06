edf6fa2de8d22e4478d37c201b3110415ee142e2
CI Automation <eng-info-dev-github-bot@okta.com>
2024-09-06 00:41:32 +0000

    Updates prod-asa

--- asa/Resources/Scripts/require.config.js
+++ asa/Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=638611668511057220'
+    urlArgs: 't=638611800119281129'
 });
--- asa/en-us/Resources/Scripts/require.config.js
+++ asa/en-us/Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=638611668405111708'
+    urlArgs: 't=638611800011296597'
 });
