b77e0aff93387ed44c8c7dfdd0aa59fbcbb0c413
CI Automation <eng-info-dev-github-bot@okta.com>
2024-11-01 21:08:43 +0000

    Updates prod-wf

--- wf/en-us/Data/Search.js
+++ wf/en-us/Data/Search.js
@@ -1,1 +1,1 @@
-define({r:1,st:'Stem',n:1,t:[0],m:[],u:['../content/topics/releasenotes/archive/wf-relnotes-2020.htm'],s:['$'],p:[['$',74]]});
+define({r:1,st:'Stem',n:1,t:[0],m:[],u:['../content/topics/workflows/connector-reference/connector-reference.htm'],s:['0'],p:[['0',2]]});
